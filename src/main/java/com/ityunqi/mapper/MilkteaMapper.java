ee392afe0620022d7d942b7dfe4f4983e4db5f16
huanghaozhe <1799330866@qq.com>
2024-03-02 14:56:52 +0800

    黄

--- src/main/java/com/ityunqi/mapper/MilkteaMapper.java
+++ src/main/java/com/ityunqi/mapper/MilkteaMapper.java
@@ -13,17 +13,17 @@ public interface MilkteaMapper {
     @Select("select * from dbyunqi.milktea")
     public List<Milktea> selectall();
 
-    @Insert("insert into dbyunqi.milktea values(null,#{milkname},#{introduce},#{kindid},#{price},#{count},#{salenumber}) ")
+    @Insert("insert into dbyunqi.milktea values(null,#{milkname},#{introduce},#{kindid},#{price},#{count},#{salenumber},#{image}) ")
     void add(Milktea milktea);
 
     @Delete("delete from dbyunqi.milktea where id=#{id}")
     void deletebyid(int id);
 
-    @Update("update dbyunqi.milktea set id=#{id},milkname=#{milkname},introduce=#{introduce},kindid=#{kindid},price=#{price},count=#{count},salenumber=#{salenumber} where id=#{id}")
+    @Update("update dbyunqi.milktea set id=#{id},milkteaname=#{milkname},introduce=#{introduce},kindid=#{kindid},price=#{price},count=#{count},salenumber=#{salenumber} where id=#{id}")
     void update(Milktea milktea);
 
 
     public List<Milktea> kindidselect(int kindid);
 
-    public List<Milktea> salenumberselect(int salenumber);
+    public List<Milktea> salenumberselect();
 }
--- src/main/java/com/ityunqi/web/servlet/Bservlet/milkteaServlet.java
+++ src/main/java/com/ityunqi/web/servlet/Bservlet/milkteaServlet.java
@@ -17,18 +17,16 @@ import java.io.IOException;
 import java.util.List;
 
 @WebServlet("/milktea/*")
+
 public class milkteaServlet extends BaseServlet {
 
     private Milkteaservice milkteaservice = new Milkteaserviceimpl();
 
     public void selectall(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
-        List<Milktea> milkteas= milkteaservice.selectall();
-        String jsonString = JSON.toJSONString(milkteas);
-        httpServletResponse.setContentType("text/json;charset=utf-8");
-        httpServletResponse.getWriter().write(jsonString);
-        Result result=Result.success(milkteas);
-        httpServletResponse.getWriter().write(JSON.toJSONString(result));
-
+        List<Milktea> milkteas = milkteaservice.selectall();
+        String jsonString = JSON.toJSONString(Result.success(milkteas));
+        httpServletResponse.setContentType("application/json;charset=utf-8");
+        httpServletResponse.getWriter().write(JSON.toJSONString(jsonString));
     }
 
     public void add(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
@@ -36,14 +34,14 @@ public class milkteaServlet extends BaseServlet {
         String params = bufferedReader.readLine();
         Milktea milktea = JSON.parseObject(params, Milktea.class);
         milkteaservice.add(milktea);
-        Result result=Result.success(milktea);
+        Result result = Result.success(milktea);
         httpServletResponse.getWriter().write(JSON.toJSONString(result));
     }
 
     public void deleteid(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         String id = httpServletRequest.getParameter("id");
         milkteaservice.deletebyid(Integer.parseInt(id));
-        Result result=Result.success();
+        Result result = Result.success();
         httpServletResponse.getWriter().write(JSON.toJSONString(result));
 
     }
@@ -65,10 +63,10 @@ public class milkteaServlet extends BaseServlet {
         milktea1.setPrice(milktea.getPrice());
 
         milktea1.setCount(milktea.getCount());
-
+        milktea1.setImage(milktea.getImage());
         milktea1.setSalenumber(milktea.getSalenumber());
         milkteaservice.update(milktea1);
-        Result result=Result.success(milktea1);
+        Result result = Result.success(milktea1);
         httpServletResponse.getWriter().write(JSON.toJSONString(result));
 
     }
@@ -81,28 +79,27 @@ public class milkteaServlet extends BaseServlet {
 
         int kindid = Integer.parseInt(_kindid);
         List<Milktea> milkteas = milkteaservice.kindidselect(kindid);
-        String jsonString = JSON.toJSONString(milkteas);
-        httpServletResponse.setContentType("text/json;charset=utf-8");
-        Result result=Result.success(milktea);
-        httpServletResponse.getWriter().write(JSON.toJSONString(result));
-
+        String jsonString = JSON.toJSONString(Result.success(milkteas));
+        httpServletResponse.setContentType("application/json;charset=utf-8");
+        httpServletResponse.getWriter().write(JSON.toJSONString(jsonString));
 
 
     }
-    public void salenumberselect(HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse)throws ServletException,IOException{
+
+    public void salenumberselect(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         BufferedReader br = httpServletRequest.getReader();
         String params = br.readLine();
         Milktea milktea = JSON.parseObject(params, Milktea.class);
-        String _salenumber = httpServletRequest.getParameter("salenumber");
-
-        int salenumber = Integer.parseInt(_salenumber);
-        List<Milktea> milkteas = milkteaservice.salenumberselect(salenumber);
-        String jsonString = JSON.toJSONString(milkteas);
+        List<Milktea> milkteas = milkteaservice.salenumberselect();
+        /*String jsonString = JSON.toJSONString(milkteas);
         httpServletResponse.setContentType("text/json;charset=utf-8");
-        Result result=Result.success(milktea);
-        httpServletResponse.getWriter().write(JSON.toJSONString(result));
-    }
+        Result result = Result.success(milktea);
+        httpServletResponse.getWriter().write(JSON.toJSONString(result));*/
+        String jsonString = JSON.toJSONString(Result.success(milkteas));
+        httpServletResponse.setContentType("application/json;charset=utf-8");
+        httpServletResponse.getWriter().write(JSON.toJSONString(jsonString));
 
+    }
 
 
 }
